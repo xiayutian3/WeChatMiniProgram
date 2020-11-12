89a1dd94ecf68c72af0940ebda8b45b06238929e
1127071993@qq.com <aini2009626>
2020-11-13 00:15:33 +0800

    49-小程序-实现再次播放跳转至指定位置功能

--- yunMusic/pages/video/video.js
+++ yunMusic/pages/video/video.js
@@ -3,7 +3,9 @@ Page({
   data: {
     videoGroupList: [],  //导航数据
     navId: '' , //导航的标识
-    videoList:[] 
+    videoList:[], //视频列表数据
+    videoId:'', // 视频id标识
+    videoUpdateTime:[], //记录m每一个video播放的时长
   },
   onLoad() {
     //获取导航数据
@@ -23,6 +25,9 @@ Page({
   async getVideoList(navId){
     let res = await request('/video/group',{id:navId})
     let index = 0;
+    if(!res.datas){
+      return
+    }
     let videoList = res.datas.map(item=>(item.id = index++,item))
     this.setData({
       videoList
@@ -58,9 +63,9 @@ Page({
   },
 
 
-  //处理视频播放
+  //处理视频播放(真机上没有问题，模拟器会出现一些问题-by 2020-11-12)
   handlePlay(event){
-    console.log("handlePlay -> event", event)
+    // console.log("handlePlay -> event", event)
     /**
      * 需求（一次只能播放一个视频）
      * 1.在点击播放事件中需要找到上一个播放的视频
@@ -72,13 +77,59 @@ Page({
      */
     //创建控制video标签的实例对象
     let vid = event.target.id
-    
-    //关闭之前的video(id,videoContent不同)
-    if(this.videoContent && this.vid !== vid){
-      this.videoContent.stop()
-    }
-    this.vid = vid
-    this.videoContent = wx.createVideoContext(vid)
-  }
 
+    // //关闭之前的video(id,videoContent不同)
+    // if(this.videoContent && this.vid !== vid){
+    //   this.videoContent.stop()
+    // }
+    // this.vid = vid
+
+    //更新需要播放的视频id
+    this.setData({
+      videoId:vid
+    })
+
+    this.videoContent = wx.createVideoContext(vid)
+    //播放视频
+    //判断之前是否有播放过，是否有播放记录，如果有，跳转至指定的位置
+    let videoItem = this.data.videoUpdateTime.find(item=>item.vid === vid)
+    if(videoItem){  // 有，跳转至指定的位置
+      this.videoContent.seek(videoItem.currentTime)
+    }
+    this.videoContent.play()
+  },
+  //处理播放时长的回调
+  handleTimeUpdate(event){
+  // console.log("handleTimeUpdate -> event", event)
+  let videoTimeObj = {
+    vid:event.currentTarget.id,
+    currentTime:event.detail.currentTime
+  }
+  let {videoUpdateTime} = this.data;
+  /**
+   * 思路：判断记录播放时长的videoUpdateTime数组中是否有当前的播放记录
+   * 1.如果有，在原来的播放记录上更新播放时间为当前的播放时间
+   * 2.如果没有，需要在数组中添加当前视频的播放对象
+   */
+  let videoItem = videoUpdateTime.find(item=>item.vid === videoTimeObj.vid)
+  if(videoItem){  //之前有
+    videoItem.currentTime = event.detail.currentTime
+  }else{ //之前没有
+    videoUpdateTime.push(videoTimeObj)
+  }
+ // 更新
+  this.setData({
+    videoUpdateTime
+  })
+  },
+  //播放结束的时候
+  handleEnded(event){
+    // console.log('结束')
+    let {videoUpdateTime} = this.data;
+    let index = videoUpdateTime.findIndex(item=>item.vid === event.currentTarget.id)
+    videoUpdateTime.splice(index,1)
+    this.setData({
+      videoUpdateTime
+    })
+  }
 })
--- yunMusic/utils/config.js
+++ yunMusic/utils/config.js
@@ -1,5 +1,5 @@
 // 服务器相关的配置
 export default {
   host:'http://localhost:3000',  //本地服务地址
-  mobileHost:'https://b31c57662414.ngrok.io'  //内网穿透地址(ngrok)（可以看成是外网地址，访问往外网穿透到内网，实现对内网的访问），用于真机上测试小程序
+  mobileHost:'https://189f918691fc.ngrok.io'  //内网穿透地址(ngrok)（可以看成是外网地址，访问往外网穿透到内网，实现对内网的访问），用于真机上测试小程序
 }
