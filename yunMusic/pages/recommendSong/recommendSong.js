5590fb3a97cef51ea139949af0b9044cdb8e151d
1127071993@qq.com <aini2009626>
2020-11-14 11:21:45 +0800

    60-小程序-底部控制区域搭建

--- yunMusic/pages/recommendSong/recommendSong.js
+++ yunMusic/pages/recommendSong/recommendSong.js
@@ -1,4 +1,7 @@
 // pages/recommendSong/recommendSong.js
+//封装后的请求
+import request from '../../utils/request'
+
 Page({
 
   /**
@@ -6,17 +9,43 @@ Page({
    */
   data: {
     day:'',//天
-    month:'' //月
+    month:'', //月
+    recommendList:[] //每日推荐的歌曲数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //判断用户书否登录
+    let userInfo = wx.getStorageSync('userInfo');
+    if(!userInfo){
+      wx.showToast({
+        title:'请先登录',
+        icon:'none',
+        success:()=>{
+          //跳转到登录界面
+          wx.reLaunch({
+            url:'/pages/login/login'
+          })
+        }
+      })
+    }
     //更新日期的状态数据
     this.setData({
       day:new Date().getDate(),
       month:new Date().getMonth()+1   //国外的月份和我们有点不一样
+    })
+
+    //获取每日推荐的数据
+    this.getrecommendList()
+  },
+  
+  // 获取每日推荐的数据
+  async getrecommendList(){
+    let res = await request('/recommend/songs')
+    this.setData({
+      recommendList:res.recommend
     })
   },
 
